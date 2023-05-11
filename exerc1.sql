9a392de89bb5341194257dc17279d3f91ff273f9
Charles <charles99na@gmail.com>
2023-05-10 23:19:31 -0300

    adicionando ativo

--- exerc1.sql
+++ exerc1.sql
@@ -13,13 +13,20 @@ create table tb_cola(
     PRIMARY KEY(id)
 );
 
-insert into tb_cola(nome, idade, data_nasc, cargo, salario) values ("Charles", 25, "1998-01-17", "Desenvolvedor", 3500.00);
-insert into tb_cola(nome, idade, data_nasc, cargo, salario) values ("Roberto", 22, "1991-06-19", "Desenvolvedor Junior", 3500.00);
-insert into tb_cola(nome, idade, data_nasc, cargo, salario) values ("João", 29, "1995-08-26", "Desenvolvedor Pleno", 5500.00);
-insert into tb_cola(nome, idade, data_nasc, cargo, salario) values ("Pedro", 19, "2002-09-11", "Marketing", 1500.00);
+insert into tb_cola(nome, idade, data_nasc, cargo, salario) values ("Charles", 25, "1998-01-17", "Desenvolvedor", 3500.00, 1);
+insert into tb_cola(nome, idade, data_nasc, cargo, salario) values ("Roberto", 22, "1991-06-19", "Desenvolvedor Junior", 3500.00, 1);
+insert into tb_cola(nome, idade, data_nasc, cargo, salario) values ("João", 29, "1995-08-26", "Desenvolvedor Pleno", 5500.00, 0);
+insert into tb_cola(nome, idade, data_nasc, cargo, salario) values ("Pedro", 19, "2002-09-11", "Marketing", 1500.00, 0);
 
 select * from tb_cola where salario > 2000.00;
 select * from tb_cola where salario < 2000.00;
 
 update tb_cola set cargo = "Desenvolvedor" where id = 4;
+
+alter table tb_cola add ativo tinyint(1);
+select * from tb_cola;
+update tb_cola set ativo = 1 where id = 1;
+update tb_cola set ativo = 0 where id = 2;
+update tb_cola set ativo = 1 where id = 3;
+update tb_cola set ativo = 0 where id = 4;
 
